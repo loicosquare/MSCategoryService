65d960eb7aa474eda5869790707f16cceefeb955
Loic Sanou <loicsan07@gmail.com>
2024-02-15 16:41:35 +0100

    Update entity adding Rating and Lombok dependency

--- src/main/java/com/tech/categoryService/external/entities/Game.java
+++ src/main/java/com/tech/categoryService/external/entities/Game.java
@@ -1,7 +1,6 @@
 package com.tech.categoryService.external.entities;
 
 import lombok.*;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,12 +19,10 @@ public class Game {
     private String platform; //(PS5, PS4, PC).
     private String developer;
     private String publisher;
-    //private String ratingId;
     private double price;
     private int gameLength;
     private String imageUrl;
-    private String enterpriseId; // Référence à l'entreprise qui a créé le jeu.
-    private String categoryId; // Référence à la catégorie à laquelle appartient le jeu.
-    //@Transient //not to be stored in db
-    private List<Rating> ratings=new ArrayList<>();
+    private String enterpriseId;
+    private String categoryId;
+    private List<Rating> ratings = new ArrayList<>();
 }
